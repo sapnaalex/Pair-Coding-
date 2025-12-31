db0696dd3e1912dd3331509addee08568ab3af45
unknown <sapna.alex03@gmail.com>
2025-12-31 11:20:09 +0530

    Binary search on a sorted array to find index of x

---
+++ Binary serach/find elements x/find_elemt_x.py
@@ -0,0 +1,20 @@
+def binary_search(nums, target):
+    n = len(nums)
+    low = 0
+    high = n-1
+
+    while low<=high:
+        mid = (low+high)//2
+
+        if(nums[mid]==target):
+            return mid
+        elif nums[mid]<target:
+            low = mid+1
+        else:
+            high = mid -1
+    return -1
+
+arr = list(map(int, input().split()))
+target = int(input())
+index = binary_search(arr, target)
+print(index)
